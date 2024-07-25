2de6e0c40ee5e89cec1aa007a0864c938fd1b87b
akda007 <akdagmr007s@gmail.com>
2024-07-25 11:16:14 -0300

    Implemented code abstraction to create dynamic components to draw the game table

--- src/pages/MainPage/components/GameComponent/index.tsx
+++ src/pages/MainPage/components/GameComponent/index.tsx
@@ -1,27 +1,55 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { Box, Stack } from "@mui/material";
-import { useState } from "react";
-import GameSquare from "../GameSquare";
+import { Component, ReactNode, useState } from "react";
+import { FieldDataBomb, FieldDataEmpty, FieldDataNumber, GameSquare, IFieldData } from "../GameSquare";
 
+function getRandomInt(min:number, max:number) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
 
+const  calculateBombs = (array: IFieldData[][], position: {x: number, y: number}):number => {
+    return 1;
+}
 
 export default function GameComponent() {
     const [columns, setColumns] = useState(8);
     const [rows, setRows] = useState(8);
+    const [bombs, setBombs] = useState(8);
+
+    const fieldMap: IFieldData[][] = Array(rows)
+        .fill(0)
+        .map(() => Array(columns).fill(new FieldDataEmpty()));
+
+    for (let i = 0; i < bombs; i++) {
+        const x = getRandomInt(0, columns);
+        const y = getRandomInt(0, rows);
+
+        fieldMap[y][x] = new FieldDataBomb();
+    }
+
+    // fieldMap.map((_, i) => {
+    //     fieldMap.map((_, j) => {
+    //         const value = calculateBombs(fieldMap, {x: j, y: i});
+
+    //         fieldMap[i][j] = new FieldDataNumber(value);
+    //     })
+    // })
 
 
-    const clickHandler = (x:number, y:number) => {
-        alert(x);
+    const clickHandler = ({ x, y }: {x:number, y:number}) => {
+        alert(`X: ${x} | Y: ${y}`);
     }
 
     return (
         <Box>
-            { Array.from({length: rows}).map( (_, i) => 
+            {fieldMap.map((row, i) => 
                 <Stack key={i} flexDirection="row" justifyContent="center">
-                    {Array.from({length: columns}).map((_ , j)=>
-                        <GameSquare key={j} clickHandler={clickHandler} x={i} y={j}/>
-                    )}
+                    {row.map((e, j) => 
+                        <GameSquare clickHandler={clickHandler} element={e} coordinates={{x: j, y: i}} key={(i+j*100)}/>
+                    )}        
                 </Stack>
             )}
         </Box>
--- src/pages/MainPage/components/GameSquare/index.tsx
+++ src/pages/MainPage/components/GameSquare/index.tsx
@@ -1,16 +1,46 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { Component, forwardRef, ReactNode } from "react";
 import { GameSquareElement } from "./styles";
+import { Box, BoxProps } from "@mui/material";
 
 type GameSquareProps = {
-    x:number,
-    y:number,
-    clickHandler: (x:number, y:number) => void
+    coordinates: {x: number, y: number}
+    clickHandler: (coordinates:{x: number, y: number}) => void;
+    element: IFieldData;
 }
 
-export default function GameSquare({x, y, clickHandler}:GameSquareProps) {
-    return (
-        <>
-            <GameSquareElement bgcolor="white" onClick={() => clickHandler(x, y)}/>
-        </>
-    )
+class IFieldData {
+    value: unknown;
+
+    constructor (value: unknown) {
+        this.value = value;
+    }
 }
+
+class FieldDataBomb extends IFieldData {
+    constructor () {
+        super("X");
+    }
+}
+
+class FieldDataEmpty extends IFieldData {
+    constructor() {
+        super("");
+    }
+}
+
+class FieldDataNumber extends IFieldData {
+    constructor(value: number) {
+        super(value);
+    }
+}
+
+const GameSquare = (props: GameSquareProps) => {
+    return (
+        <GameSquareElement onClick={() => props.clickHandler(props.coordinates)}>
+            {String(props.element.value)}
+        </GameSquareElement>
+    )
+};
+
+export { IFieldData, FieldDataBomb, FieldDataEmpty, FieldDataNumber, GameSquare }
